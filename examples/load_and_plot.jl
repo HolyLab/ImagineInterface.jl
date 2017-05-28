f0af262fc9633e2019abfd9da397272bc5b24961
Cody <codyjgreer@gmail.com>
2017-05-28 12:40:44 -0500

    Update load and plot example

--- examples/load_and_plot.jl
+++ examples/load_and_plot.jl
@@ -1,15 +1,15 @@
+#You must install Plots and UnitfulPlots in order for this to work
 using JSON, AxisArrays, UnitfulPlots, Plots
 using ImagineInterface
 
 plotlyjs()
 #gr()
 
-d = JSON.parsefile("controls_triangle.json")
-pos = parse_command(d, "positioner1")
+pos = parse_command("controls_triangle.json", "positioner1")
 allcoms = parse_commands("controls_triangle.json")
 sampsa = decompress(pos, 1, 78750)
 sampsd = decompress(getname(allcoms, "laser1") , 1, 78750)
 
 plot(sampsa) #has y units but not time
 
-plot(axisvalues(sampsa[Axis{:time}]), sampsa) #has y and time units.  Should we make this easier
+plot(axisvalues(sampsa[Axis{:time}]), sampsa) #has y and time units.  Should we make this easier?

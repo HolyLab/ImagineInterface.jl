8b98f16fc9c4d4958d6dd7a06c19565f09fd373c
Cody <codyjgreer@gmail.com>
2017-10-08 11:58:25 -0500

    Add and test a function for finding time indexes of an ordered list of values in a (possibly noisy) oscillating signal.

--- src/stack.jl
+++ src/stack.jl
@@ -313,4 +313,58 @@ function gen_2d_timeseries{TL<:HasLengthUnits, TT<:HasTimeUnits, TTI<:HasInverse
     output = Dict("positioner" => pos, "camera" => cam, "laser" => las)
     return output
 end
- 
+
+#Iterate through samps and circ list, marking the indices at which samp the samp vector crosses th thresholds in circ_list
+#circ_list is expected to be sorted in either forward or reverse order, and the first index of samps is expected to be outside the range spanned by first(circ_list) and last(circ_list)
+#After fully iterating through circ_list the search will continue on the reversed circ_list.
+#Since the endpoints of circ_list get repeated they can be counted too often due to noise.  For this reason the pad_samps argument determines how long to wait before counting another threshold crossing each time the list is reversed
+#Returns a vector-of-vectors where each vector contains the crossing indices of a particular circ_list value
+function find_circular(samps::AbstractVector, circ_list::AbstractVector, pad_nsamps::Int)
+    is_incr = true
+    reversed = false
+    rlist = reverse(circ_list)
+    if !issorted(circ_list)
+        if !issorted(rlist)
+            error("circ_list must be in increasing or decreasing order")
+        end
+        circ_list = rlist
+        is_incr = false
+        reversed = true
+    end
+    samp1 = first(samps)
+    if (is_incr && (samp1 > circ_list[1])) || (!is_incr && (samp1 < circ_list[end]))
+        error("The sample vector begins in the middle of a cycle")
+    end
+    outputs = Vector{Vector{Int}}(length(circ_list))
+    for i = 1:length(circ_list)
+        outputs[i] = Int[]
+    end
+    wait_stop = 0
+    circ_i = reversed ? length(circ_list) : 1
+    for i = 1:length(samps)
+        if i == wait_stop
+            if (is_incr && (samps[i] >= circ_list[circ_i])) || (!is_incr && (samps[i] <= circ_list[1]))
+                error("The signal had already passed threshold after waiting for pad_nsamps samples")
+            end
+        elseif i > wait_stop
+            if is_incr && (samps[i] >= circ_list[circ_i])
+                push!(outputs[circ_i], i)
+                if circ_i == length(circ_list)
+                    is_incr = false
+                    wait_stop = i + pad_nsamps
+                else
+                    circ_i += 1
+                end
+            elseif !is_incr && (samps[i] <= circ_list[circ_i])
+                push!(outputs[circ_i], i)
+                if circ_i == 1
+                    is_incr = true
+                    wait_stop = i + pad_nsamps
+                else
+                    circ_i -= 1
+                end
+            end
+        end
+    end
+    return reversed ? reverse(outputs) : outputs
+end
--- test/build_stack.jl
+++ test/build_stack.jl
@@ -1,4 +1,6 @@
 using ImagineInterface, Unitful, IntervalSets
+import ImagineInterface: find_circular
+import Unitful: s, μm
 using Base.Test
 
 ##################################BIDIRECTIONAL STACK########################################3
@@ -157,4 +159,33 @@ d3 = gen_2d_timeseries(pset, 10, exp_time, inter_exp_time, sample_rate, flash_fr
 @test length(find(x->x==1, diff(d3["laser"]))) == 10
 @test (exp_time + inter_exp_time) * 10 * sample_rate ≈ length(d3["camera"])
 
+#find_circular
+samps = repmat([1:9...; 10:-1:2...], 3)
+circ_list = [2; 4]
+pad_nsamps = 0
+i_s = find_circular(samps, circ_list, pad_nsamps)
+@test i_s[1] == find(x->x==circ_list[1], samps)
+@test i_s[2] == find(x->x==circ_list[2], samps)
+@test all(find_circular(samps, circ_list, 1) .== i_s)
+@test_throws Exception find_circular(samps, circ_list, 2)
+circ_list = [4; 2]
+@test_throws Exception find_circular(samps, circ_list, pad_nsamps) #because it doesn't begin decreasing
+samps = repmat([10:-1:2...; 1:9...], 3) #begins decreasing
+i_s = find_circular(samps, circ_list, pad_nsamps)
+@test i_s[1] == find(x->x==circ_list[1], samps)
+@test i_s[2] == find(x->x==circ_list[2], samps)
 
+samps = repmat([1:9...; 10:-1:2...], 3)
+circ_list = [2.5; 4.0] #fractional
+i_s = find_circular(samps, circ_list, pad_nsamps)
+@test i_s[1][1:2:end] == find(x->x==3, samps)[1:2:end] #on increasing sweep
+@test i_s[1][2:2:end] == find(x->x==2, samps)[2:2:end] #on decreasing sweep
+@test i_s[2] == find(x->x==circ_list[2], samps) #unaffected
+
+#again with units
+samps *= Unitful.μm
+circ_list *= Unitful.μm
+i_s = find_circular(samps, circ_list, pad_nsamps)
+@test i_s[1][1:2:end] == find(x->x==3μm, samps)[1:2:end] #on increasing sweep
+@test i_s[1][2:2:end] == find(x->x==2μm, samps)[2:2:end] #on decreasing sweep
+@test i_s[2] == find(x->x==circ_list[2], samps) #unaffected

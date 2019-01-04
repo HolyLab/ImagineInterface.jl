285a95ccbfff56e65b0a4ada9a13bd6e06af4b62
Cody-G <codyjgreer@gmail.com>
2019-01-04 16:40:12 -0600

    address PR comment: fewer anonymous functions

--- src/convenience.jl
+++ src/convenience.jl
@@ -18,7 +18,7 @@ getdaqchan(coms::AbstractVector{T}, nm::AbstractString) where{T<:ImagineSignal} 
 isdigital(com::T) where{T<:ImagineSignal} =
     isdigital(daq_channel(com), rig_name(com))
 finddigital(coms::AbstractVector{T}) where{T<:ImagineSignal} =
-    findall(x->isdigital(x), coms)
+    findall(isdigital, coms)
 getdigital(coms::AbstractVector{T}) where{T<:ImagineSignal} =
     view(coms, finddigital(coms))
 isanalog(daq_chan::AbstractString, rig::AbstractString) where{T<:ImagineSignal} =
@@ -32,48 +32,48 @@ getanalog(coms::AbstractVector{T}) where{T<:ImagineSignal} =
 
 isoutput(com::T) where{T<:ImagineSignal} = isoutput(daq_channel(com), rig_name(com))
 isinput(com::T) where{T<:ImagineSignal} = !isoutput(com)
-findoutputs(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->isoutput(x), coms)
+findoutputs(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(isoutput, coms)
 getoutputs(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findoutputs(coms))
-findinputs(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->isinput(x), coms)
+findinputs(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(isinput, coms)
 getinputs(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findinputs(coms))
 
 isfree(com::T) where{T<:ImagineSignal} = isfree(daq_channel(com), rig_name(com))
 isfixed(com::T) where{T<:ImagineSignal} = !isfree(com)
-findfree(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->isfree(x), coms)
+findfree(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(isfree, coms)
 getfree(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findfree(coms))
 findfixed(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->!isfree(x), coms)
 getfixed(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findfixed(coms))
 
 ispos(com::T) where{T<:ImagineSignal} = ispos(daq_channel(com), rig_name(com))
-findpositioners(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->ispos(x), coms)
+findpositioners(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(ispos, coms)
 getpositioners(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findpositioners(coms))
 
 isposmonitor(com::T) where{T<:ImagineSignal} = isposmonitor(daq_channel(com), rig_name(com))
-findpositionermonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->isposmonitor(x), coms)
+findpositionermonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(isposmonitor, coms)
 getpositionermonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findpositionermonitors(coms))
 
 iscam(com::T) where{T<:ImagineSignal} = iscam(daq_channel(com), rig_name(com))
-findcameras(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->iscam(x), coms)
+findcameras(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(iscam, coms)
 getcameras(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findcameras(coms))
 
 iscammonitor(com::T) where{T<:ImagineSignal} = iscammonitor(daq_channel(com), rig_name(com))
-findcameramonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->iscammonitor(x), coms)
+findcameramonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(iscammonitor, coms)
 getcameramonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findcameramonitors(coms))
 
 islas(com::T) where{T<:ImagineSignal} = islas(daq_channel(com), rig_name(com))
-findlasers(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->islas(x), coms)
+findlasers(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(islas, coms)
 getlasers(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findlasers(coms))
 
 isstim(com::T) where{T<:ImagineSignal} = isstim(daq_channel(com), rig_name(com))
-findstimuli(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->isstim(x), coms)
+findstimuli(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(isstim, coms)
 getstimuli(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findstimuli(coms))
 
 isgalvo(com::T) where{T<:ImagineSignal} = iscam(daq_channel(com), rig_name(com))
-findgalvos(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->iscam(x), coms)
+findgalvos(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(iscam, coms)
 getgalvos(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findcameras(coms))
 
 isgalvomonitor(com::T) where{T<:ImagineSignal} = isgalvomonitor(daq_channel(com), rig_name(com))
-findgalvomonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(x->isgalvomonitor(x), coms)
+findgalvomonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = findall(isgalvomonitor, coms)
 getgalvomonitors(coms::AbstractVector{T}) where{T<:ImagineSignal} = view(coms, findgalvomonitors(coms))
 
 hasmonitor(com::T) where{T<:ImagineSignal} = iscam(com) || ispos(com) || isgalvo(com)
